cfb44bd2cebc20f58430476d8166586f3cc4a7d1
KumarSantanu <livemusiccompany@gmail.com>
2024-03-22 16:05:00 +0530

    maven add

--- CaptchaUI/build.gradle.kts
+++ CaptchaUI/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -42,3 +43,20 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.appixon.captchaui"
+            artifactId = "appixon"
+            version = "1.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
+
+
+
